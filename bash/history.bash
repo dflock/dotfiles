9e02662812bc00c7648b09d3eecff9b93cd8d99b
Duncan Lock <duncan.lock@gmail.com>
2024-02-28 15:02:27 +0000

    Tweak combined history config

--- bash/history.bash
+++ bash/history.bash
@@ -10,7 +10,10 @@ shopt -s cmdhist # Save multi-line commands as one command
 export HISTSIZE=-1 # Keeps all history
 export HISTFILESIZE=-1 # Infinite file size
 export HISTTIMEFORMAT='%F %T ' # Useful timestamp format
-export HISTIGNORE="&:[ ]*:exit:clear"
+export HISTIGNORE="&:hist:?:??:[ ]*:exit:pwd:clear"
 export HISTFILE=~/.bash_eternal_history
-# Record each line as it gets issued
-[[ "$PROMPT_COMMAND" == *'history -a'* ]] || export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
+# Record each line as it gets issued:
+# -a append current session history to history file
+# -c clear current session history
+# -r read history file and append contents to current session history
+[[ "$PROMPT_COMMAND" == *'history -a; history -c; history -r'* ]] || export PROMPT_COMMAND="history -a; history -c; history -r;$PROMPT_COMMAND"
