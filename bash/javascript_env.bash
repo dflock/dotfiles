edf4f07788fc85e48ea4f53670b75726b4473be0
Duncan Lock <duncan.lock@gmail.com>
2025-04-10 12:30:09 +0100

    Added nvm support

--- bash/javascript_env.bash
+++ bash/javascript_env.bash
@@ -6,7 +6,7 @@
 
 # Run 'yarn global add' without sudo, set yarn global bin somewhere else
 # https://github.com/yarnpkg/yarn/issues/1040
-pathadd "~/.config/yarn/global/bin" 'before'
+#pathadd "~/.config/yarn/global/bin" 'before'
 
 #
 # NPM stuff
@@ -24,3 +24,8 @@ pathadd "$PNPM_HOME" 'before'
 # Preferentially run stuff from the current/local node_modules folder.
 # This should come after and yarn/npm global path adds
 pathadd './node_modules/.bin' 'before'
+
+# nvm stuff
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
